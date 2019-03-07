4233f8a78463995cfafdb935234e8d0100979352
Michail Spiridonov <spiridonov.michail1@gmail.com>
2019-03-07 09:19:15 +0100

    date comparison

--- src/main/java/ContentFormatter.java
+++ src/main/java/ContentFormatter.java
@@ -1,7 +1,14 @@
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /*import java.io.BufferedWriter;
 import java.io.File;
@@ -9,6 +16,7 @@ import java.io.FileWriter;*/
 
 class ContentFormatter {
     private static final String[] DAYS = {"Utery", "Streda", "Ctvrtek", "Patek", "Sobota", "Nedele", "Pondeli"};
+    private static Date[] dates = new Date[7];
 
     /*static void writeRawToFile(ArrayList<Message> messages) throws IOException, MessagingException {
         for(int i = 0; i<messages.size();i++){
@@ -21,6 +29,7 @@ class ContentFormatter {
 
     static ArrayList<String[]> extractShiftTimes(Message message) throws IOException, MessagingException {
         ArrayList<String[]> shifts = new ArrayList<String[]>();
+        String subject = message.getSubject();
         String content = MailExtractor.getTextFromMessage(message);
         String[] lines =content.split("\n");
         for(int i = 0; i<lines.length; i++){
@@ -32,17 +41,18 @@ class ContentFormatter {
                         String[] shift = new String[8];
                         shift[0] = lines[i+2];
                         shift[1] = lines[i+6];
-                        shift[3] = lines[i+8];
-                        shift[4] = lines[i+10];
-                        shift[5] = lines[i+12];
-                        shift[6] = lines[i+14];
-                        shift[7] = lines[i+16];
+                        shift[2] = lines[i+8];
+                        shift[3] = lines[i+10];
+                        shift[4] = lines[i+12];
+                        shift[5] = lines[i+14];
+                        shift[6] = lines[i+16];
+                        shift[7] = lines[i+18];
                         i+=20;
                         shifts.add(shift);
                     }
                 }
             }catch (ArrayIndexOutOfBoundsException e){
-                e.printStackTrace();
+                System.out.println();
             }
         }
         return shifts;
@@ -55,5 +65,28 @@ class ContentFormatter {
             System.out.println(DAYS[i]);
             System.out.println(shift[i+1]);
         }
+    }
+
+    static void getDate(String subject) throws IOException {
+        String[] parts = subject.split(" ");
+        String date = parts[3];
+        BufferedWriter bw = new BufferedWriter(new FileWriter(new File("date")));
+        bw.write(date);
+        bw.close();
+        Date date1 = null;
+        try {
+            date1 = new SimpleDateFormat("yyyy.MM.dd").parse(date);
+        } catch (ParseException e) {
+            System.out.println("wrong date");
+        }
+        for(int i=0; i<7; i++){
+            Calendar c = Calendar.getInstance();
+            c.setTime(date1);
+            c.add(Calendar.DATE, i);
+            dates[i] = c.getTime();
+        }
+        /*for(Date date2 : dates){
+            System.out.println(date2);
+        }*/
     }
 }
--- src/main/java/PizzaHutShifts.java
+++ src/main/java/PizzaHutShifts.java
@@ -1,15 +1,53 @@
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
 
 public class PizzaHutShifts {
     public static void main(String[] args) throws IOException, MessagingException {
+        isSaved();
         ArrayList<Message> shiftMails = MailExtractor.getMails();
-        ArrayList<String[]> shifts = ContentFormatter.extractShiftTimes(shiftMails.get(2));
+        ContentFormatter.getDate(shiftMails.get(shiftMails.size()-1).getSubject());
+        /*ArrayList<String[]> shifts = ContentFormatter.extractShiftTimes(shiftMails.get(shiftMails.size()-1));
         for(String[] shift : shifts){
             ContentFormatter.printShifts(shift);
+        }*/
+    }
+
+    private static void isSaved(){
+        File file = new File("date");
+        try {
+            Scanner sc = new Scanner(file);
+            Scanner ssc = new Scanner(System.in);
+            DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+            Date curDate = new Date();
+            Date savedDate = dateFormat.parse(sc.next());
+            long diff = curDate.getTime() - savedDate.getTime();
+            long diffDays = diff / (24 * 60 * 60 * 1000);
+            if(diffDays>3){
+                System.out.println("saved date is too old, downloading new mails");
+            } else {
+                System.out.println("saved date is less than 4 days old, download new mails? Y/N");
+                String s = sc.next();
+                if(s.equals("Y")){
+                    return;
+                } else {
+                    System.out.println("showing saved shifts");
+                }
+            }
+            System.out.println(diffDays);
+        } catch (FileNotFoundException e) {
+            System.out.println("no saved date found, downloading new mails");
+        } catch (ParseException e) {
+            System.out.println("wrong date saved, downloadin mails");
         }
     }
 }
