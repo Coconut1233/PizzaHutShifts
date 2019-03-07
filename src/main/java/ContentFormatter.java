d4f2ff986ff4a29603530004006af6a93c0fe5a1
Michail Spiridonov <spiridonov.michail1@gmail.com>
2019-03-07 08:13:46 +0100

    better mail extractor, shift formatting

--- src/main/java/ContentFormatter.java
+++ src/main/java/ContentFormatter.java
@@ -7,12 +7,52 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 class ContentFormatter {
+    static final String[] DAYS = {"Utery", "Streda", "Ctvrtek", "Patek", "Sobota", "Nedele", "Pondeli"};
+
     static void writeRawToFile(ArrayList<Message> messages) throws IOException, MessagingException {
         for(int i = 0; i<messages.size();i++){
             BufferedWriter bw = new BufferedWriter(new FileWriter(new File("message"+i)));
-            bw.write(PizzaHutShifts.getTextFromMessage(messages.get(i)));
+            bw.write(MailExtractor.getTextFromMessage(messages.get(i)));
             bw.close();
         }
 
+    }
+
+    static ArrayList<String[]> extractShiftTimes(Message message) throws IOException, MessagingException {
+        ArrayList<String[]> shifts = new ArrayList<String[]>();
+        String content = MailExtractor.getTextFromMessage(message);
+        String[] lines =content.split("\n");
+        for(int i = 0; i<lines.length; i++){
+            String line = lines[i];
+            char[] chars = line.toCharArray();
+            try{
+                if(chars[0] == '1'){
+                    while(i<lines.length){
+                        String[] shift = new String[8];
+                        shift[0] = lines[i+2];
+                        shift[1] = lines[i+6];
+                        shift[3] = lines[i+8];
+                        shift[4] = lines[i+10];
+                        shift[5] = lines[i+12];
+                        shift[6] = lines[i+14];
+                        shift[7] = lines[i+16];
+                        i+=20;
+                        shifts.add(shift);
+                    }
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+
+            }
+        }
+        return shifts;
+    }
+
+    static void printShifts(String[] shift){
+        System.out.println("Jmeno:");
+        System.out.println(shift[0]);
+        for(int i = 0; i<7; i++){
+            System.out.println(DAYS[i]);
+            System.out.println(shift[i+1]);
+        }
     }
 }
--- src/main/java/PizzaHutShifts.java
+++ src/main/java/PizzaHutShifts.java
@@ -1,70 +1,15 @@
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Properties;
-
-import javax.mail.*;
-import javax.mail.internet.MimeMultipart;
 
 
 public class PizzaHutShifts {
     public static void main(String[] args) throws IOException, MessagingException {
-        ArrayList<Message> shiftMails = new ArrayList<Message>();
-        Properties props = System.getProperties();
-        props.setProperty("mail.store.protocol", "imaps");
-        try {
-            Session session = Session.getDefaultInstance(props, null);
-            Store store = session.getStore("imaps");
-            store.connect("imap.gmail.com", "spiridonov.michail1@gmail.com", "fqnrgwruviewulxi");
-            System.out.println(store);
-
-            Folder inbox = store.getFolder("Inbox");
-            inbox.open(Folder.READ_ONLY);
-            Message messages[] = inbox.getMessages();
-            for(Message message:messages) {
-                if(message.getSubject().toLowerCase().contains("schedule: ph vokovice")){
-                    if(shiftMails.size()>2){
-                        break;
-                    }
-                    shiftMails.add(message);
-                }
-            }
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            System.exit(2);
+        ArrayList<Message> shiftMails = MailExtractor.getMails();
+        ArrayList<String[]> shifts = ContentFormatter.extractShiftTimes(shiftMails.get(2));
+        for(String[] shift : shifts){
+            ContentFormatter.printShifts(shift);
         }
-        ContentFormatter.writeRawToFile(shiftMails);
     }
-    static String getTextFromMessage(Message message) throws MessagingException, IOException {
-        String result = "";
-        if (message.isMimeType("text/plain")) {
-            result = message.getContent().toString();
-        } else if (message.isMimeType("multipart/*")) {
-            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
-            result = getTextFromMimeMultipart(mimeMultipart);
-        }
-        return result;
-    }
-    private static String getTextFromMimeMultipart(
-            MimeMultipart mimeMultipart)  throws MessagingException, IOException{
-        String result = "";
-        int count = mimeMultipart.getCount();
-        for (int i = 0; i < count; i++) {
-            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
-            if (bodyPart.isMimeType("text/plain")) {
-                result = result + "\n" + bodyPart.getContent();
-                break;
-            } else if (bodyPart.isMimeType("text/html")) {
-                String html = (String) bodyPart.getContent();
-                result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
-            } else if (bodyPart.getContent() instanceof MimeMultipart){
-                result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
-            }
-        }
-        return result;
-    }
-
-
 }
